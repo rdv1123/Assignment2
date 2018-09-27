9e6acc4e31432b0f6ade31bf7dba80ba3ef52263
Ronald DeLuca <rdv1123@ufl.edu>
2018-09-26 20:44:27 -0400

    Listingcontroller Prep

---
+++ src/components/Listingcontroller.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Listingcontroller extends React.Component {
+    
+    addListing(){
+        // Function that will add a listing to our data
+
+    }
+
+    deleteListing(){
+        // Function that will delete a listing from our data
+
+    }
+
+    displayListing(){
+        // Function that will display a listing from our data
+
+    }
+    
+}
+export default Listingcontroller;
